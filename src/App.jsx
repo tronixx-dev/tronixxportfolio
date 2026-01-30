ecf2e9bda6679bfc949941bc0435ea0607ffa83c
Sarmie1-TX <olaniyisamuel1000@gmail.com>
2026-01-30 14:38:58 -0800

    created

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./Pages/Home";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
 import Contact from "./Pages/Contact";
 import About from "./Pages/About";
 import Signup from "./Pages/Signup";
 import Login from "./Pages/Login";
 import Projects from "./Pages/Projects";
 
-
-
-
-
-
 function App() {
   return (
-    <>
-      <BrowserRouter>
-      <Navbar/>
-        <Routes>
-          <Route path="/home" element={<Home />} />
-           <Route path="/contact" element={<Contact/>} />
-           <Route path="/about" element={<About/>} />
-           <Route path="/signup" element={<Signup/>} />
-           <Route path="/login" element={<Login/>} />
-           <Route path="/projects" element={<Projects/>}/>
-      
-          
-         
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        {/* Redirect root to home */}
+        <Route path="/" element={<Navigate to="/home" replace />} />
+
+        <Route path="/home" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
